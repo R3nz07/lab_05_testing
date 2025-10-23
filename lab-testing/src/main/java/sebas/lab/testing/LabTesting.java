6afcfc3fad472cad4f72cc125c137d4d9465c0e7
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2025-10-22 19:10:31 -0500

    feat: exer

---
+++ lab-testing/src/main/java/sebas/lab/testing/LabTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sebas.lab.testing;
+
+/**
+ *
+ * @author jacks
+ */
+public class LabTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
